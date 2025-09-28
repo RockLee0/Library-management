842c9a487d0c4abed23056208c7a8ebe1384b948
RockLee0 <mshakil3847@gmail.com>
2025-09-28 04:34:07 -0700

    cors added

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,18 @@ import { bookRouter } from "./app/controllers/books.controller";
 import { borrowRouter } from "./app/controllers/borrow.controller";
 import { notFoundHandler } from "./app/middleware/notFound";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import cors from "cors";
 
 const app: Application = express();
+
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://your-frontend-domain.com"], // whitelist
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 app.use(express.json())
 
 //routes 
